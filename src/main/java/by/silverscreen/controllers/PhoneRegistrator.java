bf748b645dce3a067fc8ff1c4e231fedce78f7c7
Sergey Baranov <sergey.baranov@intellectsoft.net>
2017-01-26 13:27:38 +0300

    small fixes

--- src/main/java/by/silverscreen/controllers/PhoneRegistrator.java
+++ src/main/java/by/silverscreen/controllers/PhoneRegistrator.java
@@ -11,9 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.Set;
 
@@ -46,12 +53,13 @@ public class PhoneRegistrator {
         }
     }
 
-    @Secured({"ADMIN","USER"})
+    @Secured({"ROLE_ADMIN","ROLE_USER"})
     @CrossOrigin
     @RequestMapping(value = "/tokens", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody
-    Map<String, Object> getAllToken () throws RestException {
+    Map<String, Object> getAllToken (Authentication authentication) throws RestException {
         try {
+            LOG.info(authentication == null? null : (UserDetails) authentication.getPrincipal());
             Set<TokenEntity> tokens = dataService.getAllTokens();
             LOG.trace("get all tokens");
             return Ajax.successResponse(tokens);
@@ -60,13 +68,14 @@ public class PhoneRegistrator {
         }
     }
 
-    @Secured("ADMIN")
+    @PreAuthorize("hasAuthority('ADMIN')")
     @CrossOrigin
     @RequestMapping(value = "/send", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody
-    Map<String, Object> sendPush (@RequestBody PushEntity data) throws RestException {
+    Map<String, Object> sendPush (Authentication authentication, @RequestBody PushEntity data) throws RestException {
         try {
             LOG.trace("push");
+            LOG.info(authentication == null? null : (UserDetails) authentication.getPrincipal());
             if (data == null) {
                 return Ajax.emptyResponse();
             } else {
@@ -78,17 +87,41 @@ public class PhoneRegistrator {
         }
     }
 
-    @Secured({"ADMIN","USER"})
+    @Secured({"ROLE_ADMIN"})
     @CrossOrigin
     @RequestMapping(value = "/notifications", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody
-    Map<String, Object> getAllNotifications () throws RestException {
+    Map<String, Object> getAllNotifications (Authentication authentication) throws RestException {
         try {
+            LOG.info(authentication == null? null : (UserDetails) authentication.getPrincipal());
             Set<NotificationEntity> tokens = dataService.getAllNotifications();
             LOG.trace("get all notifications");
             return Ajax.successResponse(tokens);
         } catch (Exception e) {
             throw new RestException(e);
         }
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/logout", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody
+    Map<String, Object> logout (Authentication authentication) throws RestException {
+        try {
+            LOG.info(authentication == null? null : (UserDetails) authentication.getPrincipal());
+          //  authentication.
+            LOG.trace("get all notifications");
+            return Ajax.successResponse("");
+        } catch (Exception e) {
+            throw new RestException(e);
+        }
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public Map<String, Object> logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return Ajax.successResponse("");//You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 }
